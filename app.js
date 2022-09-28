95f74d14c75406bb9f3f33496e2fbb8b22dd8961
duongthucamtien123 <tienb1910154@student.ctu.edu.vn>
2022-09-28 09:57:35 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
 });
 app.use((req, res, next) => {
-    return next(new ApiError(404, "Resource not found"));
+  return next(new ApiError(404, "Resource not found"));
 });
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
+  res.json({ message: "Welcome to contact book application." });
 });
 
 module.exports = app;
